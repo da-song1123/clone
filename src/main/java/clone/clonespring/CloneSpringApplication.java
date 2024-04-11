c94cb2a68eff94d524b8e38e9d0b1bc18b088ce9
da-song1123 <dsong1123@gmail.com>
2024-04-12 08:48:27 +0900

    4.12 금 최초 push

---
+++ src/main/java/clone/clonespring/CloneSpringApplication.java
@@ -0,0 +1,13 @@
+package clone.clonespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloneSpringApplication.class, args);
+	}
+
+}
